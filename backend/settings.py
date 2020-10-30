c8aaaae64413d8cd3c1a3842f95618e5faaa54df
bshelkhonov <shelbulat@mail.ru>
2020-10-31 00:32:10 +0300

    fixed crash

--- backend/settings.py
+++ backend/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Include "apps" subfolder to PATH in order to include apps stored there
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
