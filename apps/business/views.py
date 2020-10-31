77935e31f60735d6a92d7d56175b68e575f3864b
bshelkhonov <shelbulat@mail.ru>
2020-10-31 18:48:00 +0300

    created view to send businesses

--- apps/business/views.py
+++ apps/business/views.py
@@ -1,9 +1,23 @@
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from business.models import Business
-from business.serializers import BusinessSerializer
 
 
-class BusinessesListView(generics.ListAPIView):
-    queryset = Business.objects.all()
-    serializer_class = BusinessSerializer
+class BusinessesListView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        goods = {'value': 'goods', 'label': 'Товары',
+                 'children': []}
+        services = {'value': 'services', 'label': 'Услуги',
+                    'children': []}
+
+        for obj in Business.objects.all():
+            if obj.business_type == Business.GOODS:
+                goods['children'].append({'value': obj.eng_name,
+                                          'label': obj.business_name})
+            else:
+                services['children'].append({'value': obj.eng_name,
+                                             'label': obj.business_name})
+
+        return Response([goods, services], status=200)
