6792942f06a24eccd11fe6ac7c1db9d0d931d6d7
bshelkhonov <shelbulat@mail.ru>
2020-11-01 11:35:20 +0300

    fixed bug

--- apps/location_optimizer/algorithm/algorithm.py
+++ apps/location_optimizer/algorithm/algorithm.py
@@ -6,6 +6,21 @@ from sklearn.preprocessing import MinMaxScaler
 from backend.settings import GRID_STEP, EXPLANATIONS, FEATURES
 
 
+def is_intersection(coords_1, coords_2):
+    lat_min = min(v[1] for v in coords_2)
+    lat_max = max(v[1] for v in coords_2)
+    lon_min = min(v[0] for v in coords_2)
+    lon_max = max(v[0] for v in coords_2)
+
+    for corner in coords_1:
+        lat = corner[1]
+        lon = corner[0]
+        if lat_min <= lat <= lat_max and lon_min <= lon <= lon_max:
+            return True
+
+    return False
+
+
 # unite good zone with nearest
 def unite_with_nearest(lat, lon, MULT=3.5):
     left_up = (lon - MULT * GRID_STEP, lat + MULT * GRID_STEP)
@@ -13,22 +28,36 @@ def unite_with_nearest(lat, lon, MULT=3.5):
     right_up = (lon + (MULT + 1) * GRID_STEP, lat + MULT * GRID_STEP)
     right_down = (lon + (MULT + 1) * GRID_STEP, lat - (MULT + 1) * GRID_STEP)
 
-    return geojson.Polygon([[left_up, right_up, right_down,
-                             left_down, left_up]])
+    return [left_up, right_up, right_down, left_down, left_up]
 
 
 # format to geojson
 def to_geojson(best_cells, scores, explanation):
-    features = []
+    polygons = []
     for index, cell in enumerate(best_cells):
         polygon = unite_with_nearest(cell.latitude, cell.longitude)
-        feature = geojson.Feature(geometry=polygon,
-                                  properties={
-                                              'id': index,
-                                              'score': scores[index],
-                                              'explanation': explanation[index]
-                                             })
-        features.append(feature)
+        polygons.append(polygon)
+
+    count = [True] * len(polygons)
+
+    for i in range(len(polygons) - 1):
+        for j in range(len(polygons) - 1, i, -1):
+            if count[i] and is_intersection(polygons[i], polygons[j]):
+                count[j] = False
+
+    features = []
+    index = 0
+    for ind, polygon in enumerate(polygons):
+        if count[ind]:
+            polygon = geojson.Polygon([polygon])
+            feature = geojson.Feature(geometry=polygon,
+                                      properties={
+                                        'id': index,
+                                        'score': scores[ind],
+                                        'explanation': explanation[ind]
+                                      })
+            index += 1
+            features.append(feature)
 
     return geojson.FeatureCollection(features)
 
@@ -56,7 +85,7 @@ def find_best_district(business_w, cells):
     for index in range(len(cells)):
         scores[index] = business_w @ X[index]
 
-    best_variants_ind = np.argsort(-scores)[:4]
+    best_variants_ind = np.argsort(-scores)[:8]
     best_scores = scores[best_variants_ind]
 
     if np.std(best_scores) != 0:
